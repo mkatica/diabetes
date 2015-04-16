e7af9802a66bb872b101f4304ab5f52423115bb1
Mary Katica <mkatica@gmail.com>
2015-04-16 07:52:40 -0700

    jquery updates

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 $(document).foundation();
 
 // Line Chart for run totals
-var weeklyRunTotals = {
+var weeklyGlucoseTotals = {
       labels : ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
       datasets : [
         {
@@ -11,14 +11,14 @@ var weeklyRunTotals = {
           strokeColor : "rgba(220,220,220,1)",
           pointColor : "rgba(220,220,220,1)",
           pointStrokeColor : "#fff",
-          data : [5,5,5,6,6,5,5]
+          data : [120,120,120,120,120,120,120]
         },
         {
           fillColor : "rgba(52,152,219,0.5)",
           strokeColor : "rgba(52,152,219,1)",
           pointColor : "rgba(52,152,219,1)",
           pointStrokeColor : "#fff",
-          data : [2.8,4.8,4.0,1.9,9.6,2.7,10]
+          data : [88,103,129,112,120,91,97]
         }
       ]
     }
@@ -26,6 +26,39 @@ var weeklyRunTotals = {
       scaleGridLineColor : "rgba(0,0,0,.03)"
     }
 //Get context with jQuery - using jQuery's .get() method.
-var runTotal = $("#runTotal").get(0).getContext("2d");
+var glucoseTotal = $("#glucoseTotal").get(0).getContext("2d");
 //This will get the first returned node in the jQuery collection.
-var myLineChart = new Chart(runTotal).Line(weeklyRunTotals, chartOptions);
+var myLineChart = new Chart(glucoseTotal).Line(weeklyGlucoseTotals, chartOptions);
+
+var dailyExerciseGoal = [
+  {
+    value: 10,
+    color:"#4eddc1"
+  },
+  {
+    value : 20,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 75
+};
+var thisWeek = $("#dailyExerciseGoal").get(0).getContext("2d");
+var myWeeklyGoal = new Chart(thisWeek).Doughnut(dailyExerciseGoal, options);
+
+
+var dailyExercise = [
+  {
+    value: 10,
+    color:"#4eddc1"
+  },
+  {
+    value : 90,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 75
+};
+var thisDay = $("#dailyExercise").get(0).getContext("2d");
+var myDailyGoal = new Chart(thisDay).Doughnut(dailyExercise, options);
