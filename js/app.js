38e331e9c0f095af872081860c7fccec5462b8da
Mary Katica <mkatica@gmail.com>
2015-04-16 11:47:09 -0700

    Updated buttons

--- js/app.js
+++ js/app.js
@@ -46,19 +46,24 @@ var options = {
 var thisWeek = $("#dailyExerciseGoal").get(0).getContext("2d");
 var myWeeklyGoal = new Chart(thisWeek).Doughnut(dailyExerciseGoal, options);
 
-
-var dailyExercise = [
-  {
-    value: 10,
-    color:"#4eddc1"
-  },
-  {
-    value : 90,
-    color : "#E2EAE9"
-  }
-]
-var options = {
-  percentageInnerCutout : 75
+var weeklyAverage = {
+    labels: ["40", "50", "60", "70", "80", "90", "100"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
 };
-var thisDay = $("#dailyExercise").get(0).getContext("2d");
-var myDailyGoal = new Chart(thisDay).Doughnut(dailyExercise, options);
