bc8d74c856953c1734e69a1933716fe6811f3774
Mary Katica <mkatica@gmail.com>
2015-04-23 10:25:10 -0700

    styling text

--- js/app.js
+++ js/app.js
@@ -7,23 +7,22 @@ var weeklyGlucoseTotals = {
       labels : ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
       datasets : [
         {
-          fillColor : "rgba(220,220,220,0.5)",
-          strokeColor : "rgba(220,220,220,1)",
-          pointColor : "rgba(220,220,220,1)",
-          pointStrokeColor : "#fff",
+          fillColor : "rgba(92,219,213,.30)",
+          // pointDot : false,
           data : [120,120,120,120,120,120,120]
         },
         {
-          fillColor : "rgba(52,152,219,0.5)",
-          strokeColor : "rgba(52,152,219,1)",
-          pointColor : "rgba(52,152,219,1)",
+          fillColor : "rgba(24,161,154,0.64)",
+          strokeColor : "rgba(24,161,154,1)",
+          pointColor : "rgba(24,161,154,1)",
           pointStrokeColor : "#fff",
           data : [88,103,129,112,120,91,97]
         }
       ]
     }
     var chartOptions = {
-      scaleGridLineColor : "rgba(0,0,0,.03)"
+      scaleGridLineColor : "rgba(113,101,93,.3)"
+      // scaleShowVerticalLines: false,
     }
 //Get context with jQuery - using jQuery's .get() method.
 var glucoseTotal = $("#glucoseTotal").get(0).getContext("2d");
@@ -45,25 +44,3 @@ var options = {
 };
 var thisWeek = $("#dailyExerciseGoal").get(0).getContext("2d");
 var myWeeklyGoal = new Chart(thisWeek).Doughnut(dailyExerciseGoal, options);
-
-var weeklyAverage = {
-    labels: ["40", "50", "60", "70", "80", "90", "100"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
